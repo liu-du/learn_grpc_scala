bc09ce0169bb8ad15fd04e5d73acd3edcdb45a0e
liu-du <duliujimmy@hotmail.com>
2019-10-13 21:47:21 +1100

    Client streaming

--- src/main/scala/greet/GreetClient.scala
+++ src/main/scala/greet/GreetClient.scala
@@ -5,6 +5,8 @@ import scala.concurrent.Await
 import scala.concurrent.duration._
 import scala.concurrent.ExecutionContext.global
 import common.Channel
+import io.grpc.stub.StreamObserver
+import java.util.concurrent.CountDownLatch
 
 object GreetClient extends Channel {
 
@@ -14,6 +16,38 @@ object GreetClient extends Channel {
     val request = GreetingRequest(Some(Greeting("Dwyane", "Wade")))
     val f = client.greet(request)
 
+    val latch = new CountDownLatch(1)
     println(Await.result(f, 100.seconds).result)
+
+    val responseObserver = new StreamObserver[GreetingResponse] {
+      def onNext(value: GreetingResponse): Unit = {
+        println("server send us a response")
+        println(value.result)
+      }
+      def onError(t: Throwable): Unit = {}
+      def onCompleted(): Unit = {
+        println("server notify us it's done")
+        latch.countDown()
+      }
+    }
+
+    val requestObserver = client.longGreet(responseObserver)
+
+    Thread.sleep(1000L)
+    println("streaming #1")
+    requestObserver.onNext(GreetingRequest(Some(Greeting("Dwyane", "Wade"))))
+
+    Thread.sleep(1000L)
+    println("streaming #2")
+    requestObserver.onNext(GreetingRequest(Some(Greeting("Lebron", "James"))))
+
+    Thread.sleep(1000L)
+    println("streaming #3")
+    requestObserver.onNext(GreetingRequest(Some(Greeting("Kobe", "Bryant"))))
+
+    println("I (client) am done streaming")
+    requestObserver.onCompleted()
+
+    latch.await()
   }
 }
--- src/main/scala/greet/GreetingServiceImpl.scala
+++ src/main/scala/greet/GreetingServiceImpl.scala
@@ -1,6 +1,7 @@
 package greet
 
 import scala.concurrent.Future
+import io.grpc.stub.StreamObserver
 
 object GreetingServiceImpl extends GreetingServiceGrpc.GreetingService {
   def greet(request: GreetingRequest): Future[GreetingResponse] = {
@@ -13,5 +14,29 @@ object GreetingServiceImpl extends GreetingServiceGrpc.GreetingService {
         }
       )
     )
+  }
+
+  def longGreet(
+      responseObserver: StreamObserver[GreetingResponse]
+  ): StreamObserver[GreetingRequest] = {
+    var result = "Hello "
+
+    val requestObserver =
+      new StreamObserver[GreetingRequest] {
+        def onNext(value: GreetingRequest): Unit = {
+          println("received a request")
+          result += value.greeting
+            .map(greeting => s"${greeting.firstName} ${greeting.lastName} ")
+            .getOrElse("")
+        }
+        def onError(t: Throwable): Unit = {}
+        def onCompleted(): Unit = {
+          println("client notify us it's finished")
+          responseObserver.onNext(GreetingResponse(result + "!"))
+          responseObserver.onCompleted()
+        }
+      }
+
+    requestObserver
   }
 }
